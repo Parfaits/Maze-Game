7017a4fdce644f775db551cd2178a756bc3a3b36
fwarraic <fwarraic@sfu.ca>
2020-02-11 18:51:29 -0800

    Player movement and position tested

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import model.Maze;
+import model.Player;
 
 public class Main {
     public static void main(String[] args) {
-        Maze maze = new Maze(10, 10);
-        maze.printMaze();
-        // hello JDawg
+//        Maze maze = new Maze(10, 10);
+//        maze.printMaze();
+
+
+        Player player = new Player(0, 0, true);
+        player.printPlayer();
     }
 }
