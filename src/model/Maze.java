c72256395d51cf4217a6d7fad52c662817e1eaa3
johnnyd <johnnyd@sfu.ca>
2020-02-13 17:29:57 -0800

    Bug: mazeGenerate

--- src/model/Maze.java
+++ src/model/Maze.java
@@ -5,8 +5,8 @@ import java.util.List;
 import java.util.Random;
 
 public class Maze {
-    private int length;
     private int width;
+    private int length;
     private MazeElement[][] maze;
     private MazeElement[][] mazeMask;
     private Random random = new Random();
@@ -54,49 +54,69 @@ public class Maze {
 
     private void mazeGenerate() {
         List<int[]> wallPositions = new ArrayList<>();
+        List<int[]> garbage = new ArrayList<>();
         int x = 1;
         int y = 1;
         maze[x][y] = MazeElement.PASSAGE;
         wallPositions.add(new int[]{x, y+1});
         wallPositions.add(new int[]{x+1, y});
+
         while (!wallPositions.isEmpty()) {
             int randNum = random.nextInt(wallPositions.size());
-            int[] wallAndOppositeCell =  wallPositions.get(randNum);
-            if (wallAndOppositeCell[0] > 0 && wallAndOppositeCell[1] > 0 && wallAndOppositeCell[0] < length-1 && wallAndOppositeCell[1] < width-1) {
+            int[] wall =  wallPositions.get(randNum);
+
+            if (wall[0] > 0 && wall[1] > 0 && wall[0] < length-1 && wall[1] < width-1) {
+//                System.err.println("wallPoistionsSize= " + wallPositions.size());
+
                 if (
-                        (maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] + 1] == MazeElement.PASSAGE && maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] - 1] ==
-                        MazeElement.WALL && maze[wallAndOppositeCell[0] + 1][wallAndOppositeCell[1]] == MazeElement.WALL && maze[wallAndOppositeCell[0] - 1][wallAndOppositeCell[1]] == MazeElement.WALL)
+                        (maze[wall[1] + 1][wall[0]] == MazeElement.PASSAGE && maze[wall[1] - 1][wall[0]] ==
+                        MazeElement.WALL && maze[wall[1]][wall[0] + 1] == MazeElement.WALL && maze[wall[1]][wall[0] - 1] == MazeElement.WALL)
 
-                || (maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] - 1] == MazeElement.PASSAGE && maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] + 1] ==
-                        MazeElement.WALL && maze[wallAndOppositeCell[0] + 1][wallAndOppositeCell[1]] == MazeElement.WALL && maze[wallAndOppositeCell[0] - 1][wallAndOppositeCell[1]] == MazeElement.WALL)
+                || (maze[wall[1] - 1][wall[0]] == MazeElement.PASSAGE && maze[wall[1] + 1][wall[0]] ==
+                        MazeElement.WALL && maze[wall[0] + 1][wall[1]] == MazeElement.WALL && maze[wall[0] - 1][wall[1]] == MazeElement.WALL)
 
-                || (maze[wallAndOppositeCell[0] + 1][wallAndOppositeCell[1]] == MazeElement.PASSAGE && maze[wallAndOppositeCell[0] - 1][wallAndOppositeCell[1]] ==
-                        MazeElement.WALL && maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] + 1] == MazeElement.WALL && maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] - 1] == MazeElement.WALL)
+                || (maze[wall[1]][wall[0] + 1] == MazeElement.PASSAGE && maze[wall[1]][wall[0] - 1] ==
+                        MazeElement.WALL && maze[wall[1] + 1][wall[0]] == MazeElement.WALL && maze[wall[1] - 1][wall[0]] == MazeElement.WALL)
 
-                || (maze[wallAndOppositeCell[0] - 1][wallAndOppositeCell[1]] == MazeElement.PASSAGE && maze[wallAndOppositeCell[0] + 1][wallAndOppositeCell[1]] ==
-                        MazeElement.WALL && maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] + 1] == MazeElement.WALL && maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] - 1] == MazeElement.WALL)
+                || (maze[wall[1]][wall[0] - 1] == MazeElement.PASSAGE && maze[wall[1]][wall[0] + 1] ==
+                        MazeElement.WALL && maze[wall[1] + 1][wall[0]] == MazeElement.WALL && maze[wall[1] - 1][wall[0]] == MazeElement.WALL)
 
 
                 ) {
 
-                    maze[wallAndOppositeCell[0]][wallAndOppositeCell[1]] = MazeElement.PASSAGE;
+//                    System.err.println("BRO WHY");
+                    maze[wall[1]][wall[0]] = MazeElement.PASSAGE;
 
-                    if (maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] + 1] == MazeElement.WALL) {
-                        wallPositions.add(new int[]{wallAndOppositeCell[0], wallAndOppositeCell[1] + 1});
+                    if (wall[1] + 1 < length-1 && maze[wall[1] + 1][wall[0]] == MazeElement.WALL) {
+                        System.err.println("add wall[1] + 1 = " + (wall[1] + 1));
+                        int[] newWall = {wall[0], wall[1] + 1};
+                        if (!isInGarbage(garbage, newWall)) {
+
+                            wallPositions.add(newWall);
+                        }
                     }
-                    if (maze[wallAndOppositeCell[0]][wallAndOppositeCell[1] - 1] == MazeElement.WALL) {
-                        wallPositions.add(new int[]{wallAndOppositeCell[0], wallAndOppositeCell[1] - 1});
+                    if (wall[1] - 1 > 0 && maze[wall[1] - 1][wall[0]] == MazeElement.WALL) {
+                        System.err.println("add wall[1] - 1 = " + (wall[1] - 1));
+                        wallPositions.add(new int[]{wall[0], wall[1] - 1});
                     }
-                    if (maze[wallAndOppositeCell[0] + 1][wallAndOppositeCell[1]] == MazeElement.WALL) {
-                        wallPositions.add(new int[]{wallAndOppositeCell[0] + 1, wallAndOppositeCell[1]});
+                    if (wall[0] + 1 < width-1 && maze[wall[1]][wall[0] + 1] == MazeElement.WALL) {
+                        System.err.println("add wall[0] + 1 = " + (wall[0] + 1));
+                        wallPositions.add(new int[]{wall[0] + 1, wall[1]});
                     }
-                    if (maze[wallAndOppositeCell[0] - 1][wallAndOppositeCell[1]] == MazeElement.WALL) {
-                        wallPositions.add(new int[]{wallAndOppositeCell[0] - 1, wallAndOppositeCell[1]});
+                    if (wall[0] - 1 > 0 && maze[wall[1]][wall[0] - 1] == MazeElement.WALL) {
+                        System.err.println("add wall[0] - 1 = " + (wall[0] - 1));
+                        wallPositions.add(new int[]{wall[0] - 1, wall[1]});
                     }
                 }
-                wallPositions.remove(wallAndOppositeCell);
+//                wallPositions.remove(wall);
             }
-            wallPositions.remove(wallAndOppositeCell);
+            wallPositions.remove(wall);
+        }
+    }
+
+    private boolean isInGarbage(List<int[]> garbage, int[] newWall) {
+        for (int[] wallPos : garbage) {
+            
         }
     }
 
