b29d0e79e6064f1c4f5accd1c124eb520becf3cb
fwarraic <fwarraic@sfu.ca>
2020-02-25 16:39:35 -0800

    Updated UI for question mark

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import ui.TextUI;
 public class Main {
     public static void main(String[] args) {
         MazeGame game = new MazeGame();
-        game.init(18, 20);
+        game.init(15, 20);
         TextUI textUI = new TextUI(game);
         textUI.show();
 
